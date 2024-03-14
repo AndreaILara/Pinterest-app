8403cd4532dc1f6749b359beaa24573c100499f8
AndreaILara <andreainigolara@hotmail.com>
2024-03-14 22:08:33 +0100

    MESSAGE

--- main.js
+++ main.js
@@ -25,45 +25,28 @@ const searchPics = (apiUrl, key, query) => {
   if (query) {
     fetch(`${apiUrl}search/photos?query=${query}&client_id=${key}`)
       .then((res) => {
-        console.log(res);
+        if (!res.ok) {
+          throw new Error('Error en la solicitud: ' + res.status);
+        }
         return res.json();
       })
       .then((bodyRes) => {
-        console.log(bodyRes);
-
-
-        mainGallery.innerHTML = '';
-
-        if (bodyRes.results && bodyRes.results.length > 0) {
-
-          if (noImagesMessage) {
-            noImagesMessage.remove();
-          }
-
-
-          populateGallery(mainGallery, bodyRes.results);
-        } else {
-
-          noImagesMessage.innerHTML =
-            'No se encontraron resultados';
-          noImagesMessage.style.display = 'flex';
-          noImagesMessage.style.alignItems = 'center';
-          noImagesMessage.style.justifyContent = 'center';
-
-          mainGallery.appendChild(noImagesMessage);
+        if (!bodyRes || bodyRes.errors || (bodyRes.results && bodyRes.results.length === 0)) {
+          const errorMessage = document.createElement('h2');
+          errorMessage.textContent = 'No se encontraron imágenes que coincidan con tu búsqueda';
+          errorMessage.classList.add('centered-message', 'no-images-message');
+          mainGallery.innerHTML = ''; // Limpiar contenido anterior
+          mainGallery.appendChild(errorMessage);
+          return;
         }
+        mainGallery.innerHTML = ''; // Limpiar contenido anterior
+        populateGallery(mainGallery, bodyRes.results);
       })
       .catch((err) => {
         const errorMessage = document.createElement('h2');
-        errorMessage.textContent = 'Error: ' + err;
-
-
-        mainGallery.innerHTML = '';
-
-        errorMessage.style.display = 'flex';
-        errorMessage.style.alignItems = 'center';
-        errorMessage.style.justifyContent = 'center';
-
+        errorMessage.textContent = 'Error: ' + err.message;
+        errorMessage.classList.add('centered-message', 'error-message');
+        mainGallery.innerHTML = ''; // Limpiar contenido anterior
         mainGallery.appendChild(errorMessage);
       });
   }
@@ -72,9 +55,9 @@ const searchPics = (apiUrl, key, query) => {
 searchInput.addEventListener('change', (e) =>
   searchPics(UNSPLASH_API, USER_KEY, e.target.value)
 );
+
 searchInput.addEventListener('input', (e) => {
   const query = e.target.value.trim();
-
   if (query === '') {
 
     firstLoad();
@@ -83,6 +66,5 @@ searchInput.addEventListener('input', (e) => {
     searchPics(UNSPLASH_API, USER_KEY, query);
   }
 });
-
 
 firstLoad(UNSPLASH_API, USER_KEY);
